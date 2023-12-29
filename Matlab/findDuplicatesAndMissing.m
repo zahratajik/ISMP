94a541151927ec38baf97b0d3e5e1f5bab833a78
zahratajik <75752814+zahratajik@users.noreply.github.com>
2023-12-29 11:39:01 +0330

    Update findDuplicatesAndMissing.m

--- Matlab/findDuplicatesAndMissing.m
+++ Matlab/findDuplicatesAndMissing.m
@@ -1,14 +1,12 @@
 function [repeated, missing] = findDuplicatesAndMissing(array)
-    repeated = []; % آرایه‌ای برای ذخیره اعداد تکراری
-    missing = []; % آرایه‌ای برای ذخیره اعداد گم شده
+    repeated = []; 
+    missing = []; 
 
     for i = 1:length(array)
-        % بررسی تکراری بودن عدد
         if sum(array == array(i)) > 1 && ~ismember(array(i), repeated)
             repeated = [repeated, array(i)];
         end
         
-        % بررسی عدد گم شده
         if ~ismember(i, array)
             missing = [missing, i];
         end
