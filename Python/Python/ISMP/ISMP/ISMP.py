615d7c6beb8c51a12f2a11be980c764215a0e633
zahratajik <75752814+zahratajik@users.noreply.github.com>
2024-01-16 17:24:49 -0800

    added rankdown, padarray, label_of_patch to code.

--- Python/Python/ISMP/ISMP/ISMP.py
+++ Python/Python/ISMP/ISMP/ISMP.py
@@ -1,13 +1,15 @@
 # Install Library
-#pip install networkx[all]
-
+# pip install networkx[all]
+# pip install scikit-image
 
 """Import Library"""
 
 from astropy.utils.data import get_pkg_data_filename
 from matplotlib.colors import ListedColormap
+from skimage.measure import regionprops
 import matplotlib.pyplot as plt
 from scipy.io import savemat
+from scipy import ndimage, io
 from astropy.io import fits
 import networkx as nx
 import numpy as np
@@ -16,37 +18,35 @@ import warnings
 
 
 
-
 """Extraction of Connections"""
 
-def start_end_lines(im_, n2_, m2_, threshold_):
+def start_end_lines(im_, threshold_):
 
     """
     Calculating the Initial and Final Positions of Each Line:
 
     . im (array) = Image Magnetogram.
-    . m2 and n2 (int) =  m2 and n2 are  Number of rows and columns, respectively.
-    . i1 and j1 (array) = Indices of positive cells in magnetogram.
-    . i and j (array) = Indices of negitive cells in magnetogram.
-
-    Return :
+    . threshold_ (int) =  threshold_.
+    
+    output :
     .  I (array) : Coordinates the beginning and Ending Positions of Lines.
 
     """
+    m2, n2 = im.shape                  # m2 and n2 are  number of rows and columns, respectively
     w1_ = np.where(im_ > threshold_)      #positive cells with values greater than a threshold
     w2_ = np.where(im_ < -threshold_)     #negative cells with values smaller than a threshold
     i1_, j1_ = w1_[0], w1_[1]              #indices of positive cells
     i_, j_ = w2_[0], w2_[1]                #indices of negative cells
-    numSamples = 2 * max(m2_, n2_) # number of points for creating a line
+    numSamples = 2 * max(m2, n2) # number of points for creating a line
     im_[w2_] = np.abs(im_[w2_])
     I = np.zeros((len(i1_), 2), dtype=object)
     I[0,:] = 0
     for k in range(len(i1_)):
         p = 0
         end_ = [] #counter
-        x1, y1 = j1_[k], m2_ - i1_[k] + 1    #line start point coordinates
+        x1, y1 = j1_[k], m2 - i1_[k] + 1    #line start point coordinates
         for q in range(len(i_)):
-            x2, y2 = j_[q], m2_ - i_[q] + 1    #line end point coordinates
+            x2, y2 = j_[q], m2 - i_[q] + 1    #line end point coordinates
 
 
             #Recognition of Pixels
@@ -64,7 +64,7 @@ def start_end_lines(im_, n2_, m2_, threshold_):
 
 
             # Indices of pixels laied on the line. Note, start and end points are excluded.
-            ind_x1 = m2_ - finaly + 1
+            ind_x1 = m2 - finaly + 1
             ind_x = ind_x1.astype(int)
             ind_y1 = finalx
             ind_y = ind_y1.astype(int)
@@ -95,19 +95,20 @@ def start_end_lines(im_, n2_, m2_, threshold_):
 
 """Weighted Graph's Adjacency Matrix"""
 
-def create_adj_matrix(m2_, n2_, im_, I_):
+
+def create_adj_matrix( im_, I_):
     """
     Calculating Adjacency Matrix :
-    . m2 and n2 (int) =  m2 and n2 are  Number of Rows and Columns, Respectively.
+
     . im (array) =  Image Magnetogram
     . I (array) = Coordinates the beginning and Final Positions of Lines
 
-    Return:
+    output:
     . adj (array)= Adjacency Matrix
 
     """
-
-    N_pix = m2_ * n2_                    # number of pixel in each image
+    m2, n2 = im.shape                  # m2 and n2 are  number of rows and columns, respectively.
+    N_pix = m2 * n2                    # number of pixel in each image
     adj_ = np.zeros((N_pix, N_pix)) # adjacancy matrix
     for k in range(len (sum(I_[:,0] == '' for i in I_))):
         sp = I_[k][0] # start point linear index
@@ -121,52 +122,241 @@ def create_adj_matrix(m2_, n2_, im_, I_):
 
 """Degree of Nodes"""
 
-def degree_of_node(adj_, m2_, n2_):
+def degree_of_node(adj_,im_):
     """
     Calculating Degree of Nodes:
-    . adj (array) = Adjacency matrix
-    . m2 and n2 (int) =  m2 and n2 are  Number of Rows and Columns, Respectively.
-
-    Return:
+    . adj (array) = Adjacency matrix.
+    . im (array) =  Image Magnetogram
+    output:
     . edge_ (array): Degree of Node.
 
     """
+    m2, n2 = im.shape                  # m2 and n2 are  number of rows and columns, respectively
     edge = np.sum(adj_, axis=1) #Degree of node
-    edge_ = edge.reshape((m2_, n2_)) #Reshape degree of node to image size.
+    edge_ = edge.reshape((m2, n2)) #Reshape degree of node to image size.
     return edge_
+
 
 """Page-Rank"""
 
-def page_rank(G_, m2_, n2_):
+def page_rank(G_,im_):
     """
     Calculating Page-Rank:
     . G (Diagraph) = Graph
-    . m2 and n2 (int) =  m2 and n2 are  Number of Rows and Columns, Respectively.
+    . im (array) =  Image Magnetogram.
 
     Return:
     . pr_ (array) = Page-Rank
     """
+    m2, n2 = im.shape                  # m2 and n2 are  number of rows and columns, respectively
     pr=nx.pagerank(G_, alpha=0.85)
     new_pr = list(pr.items())  #  convert page-rank to array
     pr_arr = np.array(new_pr)
-    pr_ = pr_arr[:,1].reshape((m2_, n2_)) # Reshape Page-Rank to image size
+    pr_ = pr_arr[:,1].reshape((m2, n2)) # Reshape Page-Rank to image size
     return pr_
+
+
+"""Label of patches"""
+
+def label_of_patch(edge):
+
+    """
+      Determining various information for each patch, such as the center coordinate, area,
+      coordinates of pixels within the patch, and labeling:
+      input:
+
+      . edge (array) = Degree of Node.
+      output:
+
+      . Lp (list) =  Includes  center coordinate, area, coordinates of pixels within the patch, and labeling.
+    """
+    edge_pos = np.maximum(edge, 0)
+    edge_neg = np.minimum(edge, 0)
+    threshold_value = 0  # Adjust this value based on your image
+    binary_image_pos = (edge_pos > threshold_value).astype(int)
+
+    # Perform connected component labeling
+    L1, num1 = ndimage.label(binary_image_pos,structure=np.ones((3,3)))
+
+    # Initialize a list to store properties of each patch
+    LPP = []
+
+    # Iterate through each labeled patch
+    for ij in range(1, num1 + 1):
+        # Find the coordinates of pixels in the current patch
+        y, x = np.where(L1 == ij)
+
+        # Calculate centroid
+        s_pos = regionprops(L1)
+        cc_pos = s_pos[ij - 1].centroid
+
+        # Store properties in the patches_properties list
+        pos_patch = {
+            'label': ij,
+            'x': x.tolist(),  # Convert to list for better compatibility
+            'y': y.tolist(),
+            'area': len(x),
+            'center_x': cc_pos[1],
+            'center_y': cc_pos[0]
+        }
+
+        LPP.append(pos_patch)
+
+    binary_image_neg = (np.abs(edge_neg)> threshold_value).astype(int)
+
+    # Perform connected component labeling
+    L2, num2 = ndimage.label(binary_image_neg,structure=np.ones((3,3)))
+
+    # Initialize a list to store properties of each patch
+    LNP = []
+
+    # Iterate through each labeled patch
+    for ij in range(1, num2 + 1):
+        # Find the coordinates of pixels in the current patch
+        y, x = np.where(L2 == ij)
+
+        # Calculate centroid
+        s_neg = regionprops(L2)
+        cc_neg = s_neg[ij - 1].centroid
+
+        # Store properties in the patches_properties list
+        neg_patch = {
+            'label': ij,
+            'x': x.tolist(),  # Convert to list for better compatibility
+            'y': y.tolist(),
+            'area': len(x),
+            'center_x': cc_neg[1],
+            'center_y': cc_neg[0]
+        }
+        LNP.append(neg_patch)
+
+    LP=LPP+LNP
+
+
+    # Sort the structure according to values in ascending order
+    LP.sort(key=lambda patch: patch['center_x'])
+
+    # Label number
+    for i, patch in enumerate(LP):
+      patch['label'] = i+1
+    return LP
+
+""" pad_array"""
+
+def pad_array(arr, all):
+
+   """
+      Changing the size of the matrix:
+
+      input:
+      . arr (array) = Degree of Node.
+      . all (int)= defult 1.
+
+      output:
+      . arr (array) = Matrix with new size.
+    """
+   if all == 1:
+      arr1 = np.zeros((arr.shape[0]+1, arr.shape[1]))  #adds a row to top
+      arr1[1:, :] = arr
+      arr = arr1
+   if all == 1:
+      arr1 = np.zeros((arr.shape[0]+1, arr.shape[1])) #adds a row to bottom
+      arr1[:-1, :] = arr
+      arr = arr1
+   if all == 1:
+      arr1 = np.zeros((arr.shape[0], arr.shape[1]+1)) #adds col before
+      arr1[:, 1:] = arr
+      arr = arr1
+   if all == 1:
+      arr1 = np.zeros((arr.shape[0], arr.shape[1]+1)) # adds col after
+      arr1[:, :-1] = arr
+      arr = arr1
+   return(arr)
+
+
+"""rankdown"""
+
+def rankdown(Image, threshold):
+
+  """
+      Get the borders of the patches:
+
+      input:
+      . Image (array) = Magnrtogram.
+      . threshold (int)= Threshold.
+
+      output:
+      . mask (array): Boundaries matrix.
+
+  """
+  img = pad_array (Image,1)
+  if threshold < 1:
+    threshold = 0.1 * max(abs(img.ravel()))
+  nx = img.shape[1]
+  ny = img.shape[0]
+  fnx = np.fix (img.shape[1])
+  mask = np.zeros((img.shape[0], img.shape[1]))
+  offsets = [0, 1, ny+1, ny, ny-1, -1, -ny-1, -ny, -ny+1]
+  nloop = 1
+  for q2 in range(0, nloop):
+    tempimg= np.zeros((img.shape[0], img.shape[1]))
+    if q2 == 0:
+        tempimg = img.copy ()
+    else:
+        tempimg = img * (-1)
+    sor, num = np.sort(tempimg.flatten()), np.argsort(tempimg.flatten())
+    ranks = np.flip(num)
+    zer_find = np.where(tempimg < threshold)
+    n_zer_find = len (zer_find[0])
+    n_check = int(fnx * ny - n_zer_find)
+    tempmask = np.zeros((ny, nx))
+    ranks= ranks.astype(int)
+    tempmask.ravel()[ranks] = np.where(img.ravel() == img.ravel())
+    if n_zer_find != 0:
+      tempmask[zer_find[0],zer_find[1]] = n_check
+    current_label = 1
+    for q1 in range(1, n_check):
+      q1=1
+      maxnbr, maxpix = np.max(tempimg.ravel()[ranks[q1] + offsets]), np.argmax(tempimg.ravel()[ranks[q1] + offsets])
+      nbrlabels = tempmask.ravel()[ranks[q1] + offsets]
+      if len(str(maxpix)) != 0:
+        tempmask.ravel()[ranks[q1]] = nbrlabels[maxpix]
+      else:
+        tempmask.ravel()[ranks[q1]] = current_label
+        current_label += 1
+      if q2 == 1:
+        tempmask = tempmask + np.max(mask)
+      tempmask[zer_find] = 0
+      if q2 == 1:
+          tempmask = -tempmask
+      mask = mask + tempmask
+  img = img[1:-1, 1:-1]
+  mask = mask[1:-1, 1:-1]
+  mask = np.fix(mask)
+  return mask, img
+
 
 """ Plot"""
 
-def Plot(pr_, edge_, im1_):
+def Plot (pr_,edge_,im1,LP):
+
     """
     Plot :
-    . pr_ (array) = Page-Rank
-    . edge_ (array) = Degree of Node
-    . im1 (array) = Image Magnetogram
+    input:
+
+    . pr_ (array) = Page-Rank.
+    . edge_ (array) = Degree of Node.
+    . im1 (array) = Image Magnetogram.
+
+    output:
+    . Image
     """
-    fig, axs = plt.subplots(1, 3, figsize=(17,3))
+    fig, axs = plt.subplots(1, 3, figsize=(12,3))
     plt.gca().set_aspect('equal', adjustable='box')
 
 
     im1_cmap = plt.get_cmap('gray')
-    im1_im = axs[0].imshow(im1_, cmap=im1_cmap, vmin=-60, vmax=60)
+    im1_im = axs[0].imshow(im1, cmap=im1_cmap, vmin=-60, vmax=60)
     axs[0].set_xlabel('Solar-X', fontsize=18, fontname='Times New Roman')
     axs[0].set_ylabel('Solar-Y', fontsize=18, fontname='Times New Roman')
     axs[0].set_title('2013.11.01-01:19:13', fontsize=18, fontname='Times New Roman')
@@ -183,6 +373,21 @@ def Plot(pr_, edge_, im1_):
     axs[1].axis('on')
     axs[1].invert_yaxis()
     axs[1].tick_params(axis='both', which='major', labelsize=18)
+    axs[1].contour(mask1, levels=[1], linewidths=2, colors='r')
+    axs[1].contour(mask2, levels=[1], linewidths=2, colors='r')
+    # Assuming patches_properties is a list of dictionaries
+    b1 = []
+    c1 = []
+    for i in LP:
+        b1.append(i['center_x'])
+        c1.append(i['center_y'])
+        a1=(i['label'])
+    b1 = np.array(b1)
+    c1 = np.array(c1)
+    for i in range(0,len(LP)):
+        axs[1].text(round(b1[i]), round(c1[i]),str(i+1),
+             horizontalalignment='center', verticalalignment='center',
+             color='b', fontsize=8, fontname='Times New Roman', fontweight='bold')
 
 
     pr_cmap = plt.get_cmap('pink')
@@ -197,7 +402,6 @@ def Plot(pr_, edge_, im1_):
     plt.show()
 
 
-
 def run(FitsFile, threshold_=18):
 
     """
@@ -210,13 +414,16 @@ def run(FitsFile, threshold_=18):
         Threshold for magnetic field, in Gauss. The default is 18.
     """
     
-    im = fits.getdata(FitsFile)  # Load data
-    im1 = np.copy(im)
-    m2, n2 = im.shape  # m2 and n2 are the number of rows and columns, respectively
-    threshold_=18
-    I = start_end_lines(im, n2, m2, threshold_)  # Extraction of connections
-    adj = create_adj_matrix(m2, n2, im, I)  # Weighted graph's adjacency matrix
-    edge_ = degree_of_node(adj, m2, n2)  # Degree of node
+    im = fits.getdata(FitsFile)  # Load data.
+    im_copy = np.copy(im)
+    m2, n2 = im.shape  # m2 and n2 are the number of rows and columns, respectively.
+    threshold_ = 18
+    I = start_end_lines(im, threshold_)  # Extraction of connections.
+    adj = create_adj_matrix(m2, n2, im, I)  # Weighted graph's adjacency matrix.
+    edge_ = degree_of_node(adj, m2, n2)  # Degree of node.
     G = nx.DiGraph(adj)  # Graph
-    pr_ = page_rank(G, m2, n2)  # Page-Rank
-    Plot(pr_, edge_, im1)
+    pr_ = page_rank(G, m2, n2)  # Page-Rank.
+    LP = label_of_patch(edge_) 
+    mask1, image=rankdown(im_copy, threshold)
+    mask2, image=rankdown(-im_copy, threshold)
+    Plot(pr_, edge_, im_copy, LP)
